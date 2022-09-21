c6836967fd1ec352cd6b6ed14bed32faf3a14be1
Jocelyn <43210532+jocefr@users.noreply.github.com>
2022-09-21 14:37:34 -0500

    fixing?

--- _includes/goto.js
+++ _includes/goto.js
@@ -1,5 +1,9 @@
+document.getElementById("go-now").addEventListener("click", goToHymn);
+
 function goToHymn() {
     var hymnNumber = document.getElementById("number").value;
     var link = "hymns/" + hymnNumber + ".html";
+    console.log(link);
     window.location.href = link;
 }
+
